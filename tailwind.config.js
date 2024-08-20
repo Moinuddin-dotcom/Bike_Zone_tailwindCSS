9f8ae3b532dca51260e18f5f6118ac444ee7d920
Moinuddin-dotcom <moinchy7@gmail.com>
2024-08-20 22:56:45 +0600

    Bike zone clone with tailwind and daisy UI

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        background: 'white',
+      }
+    },
+  },
+  plugins: [],
+}
+
